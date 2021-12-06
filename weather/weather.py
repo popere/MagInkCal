f6d84edf3754e99263d2b27c9a32a8e3bd821d4f
popere <popere@irontec.com>
2021-12-06 23:37:31 +0100

    fix

--- weather/weather.py
+++ weather/weather.py
@@ -13,11 +13,13 @@ class WeatherHelper:
         if os.path.exists(self.currPath + '/api.key'):
             with open(self.currPath + '/api.key', 'r') as token:
                 apiKeyFile = token.read().replace('\n', '')
-        query = {'lat': lat, 'lon': lon, 'units': units, 'appid': apiKeyFile}
-        self.weatherDaily = requests.get("https://api.openweathermap.org/data/2.5/onecall", params=query)
-        self.logger.info('weather recovered sucessfully')
+                self.query = {'lat': lat, 'lon': lon, 'units': units, 'appid': apiKeyFile}
+                self.logger.info('weather initializedrecovered sucessfully')
 
     def weatherDaily(self):
-        self.logger.warn('weather: ' + json.dumps(self.weatherDaily.json()))
-
-        return self.weatherDaily.json()['daily']
+        if self.query:
+            weather = requests.get("https://api.openweathermap.org/data/2.5/onecall", params=self.query).json()
+            self.logger.warn('weather: ' + json.dumps(weather))
+            return weather['daily']
+        else:
+            return None
