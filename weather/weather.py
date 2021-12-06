a5859d7f60a7e25c06b842e3e42e0429479d3a07
popere <popere@irontec.com>
2021-12-06 23:19:32 +0100

    fix

--- weather/weather.py
+++ weather/weather.py
@@ -12,7 +12,7 @@ class WeatherHelper:
         self.currPath = str(pathlib.Path(__file__).parent.absolute())
         if os.path.exists(self.currPath + '/api.key'):
             with open(self.currPath + '/api.key', 'r') as token:
-                apiKeyFile = token.read()
+                apiKeyFile = token.read().replace('\n', '')
         query = {'lat': lat, 'lon': lon, 'units': units, 'appid': apiKeyFile}
         self.logger.warn(json.dumps(query))
         self.weather = requests.get("https://api.openweathermap.org/data/2.5/onecall", params=query).json()
