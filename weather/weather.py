0f9daa53dc37e608610207fd682e69e13025a0f3
popere <popere@irontec.com>
2021-12-06 23:08:30 +0100

    fix

--- weather/weather.py
+++ weather/weather.py
@@ -1,12 +1,19 @@
 import logging
 import requests
+import pathlib
+import os.path
+
+
 
 class WeatherHelper:
 
     def __init__(self, lat, long, units):
         # Initialise the display
         self.logger = logging.getLogger('maginkcal')
-        apiKeyFile = open('api.key')
+        self.currPath = str(pathlib.Path(__file__).parent.absolute())
+        if os.path.exists(self.currPath + '/api.key'):
+            with open(self.currPath + '/api.key', 'rb') as token:
+                apiKeyFile = token
         self.logger.warn('apiKeyFile: ' + apiKeyFile)
         query = {'lat': lat, 'long': long, 'units': units, 'api': apiKeyFile}
         self.weather = requests.get("https://api.openweathermap.org/data/2.5/onecall", params=query).json()
