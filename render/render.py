1884b4112dc7894bfae1a4afb50b4e7c4a668e48
popere <popere@irontec.com>
2021-12-07 13:14:24 +0100

    fix

--- render/render.py
+++ render/render.py
@@ -206,8 +206,6 @@ class RenderHelper:
                 cal_events_text += '<div class="event text-muted">' + str(len(calList[i]) - maxEventsPerDay) + ' more'
 
             cal_events_text += '</li>\n'
-            print(str(i))
-            print(str(((i + 1) % 7) == 0 and i != 1))
             if (((i+1) % 7) == 0 and i != 1) :
                 cal_events_text += '</ol>\n<ol class="days list-unstyled">\n'
         cal_events_text += '</ol>\n'
