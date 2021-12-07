6568ef8aa394668584a3e0656835ee98be5b6741
popere <popere@irontec.com>
2021-12-07 13:28:36 +0100

    fix

--- render/render.py
+++ render/render.py
@@ -161,7 +161,7 @@ class RenderHelper:
         weather = calDict['weather']
         weatherText = '<span>Bilbao <span class="now">' + str(round(weather['current']['temp'])) + '°C</span></span>\n'
         weatherText += '<span><img class="icon" src="http://openweathermap.org/img/wn/' + weather['daily'][0]['weather'][0]['icon'] +'@2x.png"></img></span>\n'
-        weatherText += '<span>' + str(round(weather['daily'][0]['temp']['max'])) + '°C / ' + str(round(weather['daily'][0]['temp']['min'])) + '°C</span>\n'
+        weatherText += '<span>' + str(round(weather['daily'][0]['temp']['max'])) + '°C / ' + str(round(weather['daily'][0]['temp']['min'])) + '°C </span>\n'
         weatherText += '<span>' + str(round(weather['daily'][0]['rain'])) + 'mm</div>\n'
 
         # Populate the day of week row
