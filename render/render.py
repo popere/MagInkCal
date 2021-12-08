dc9c46e7f1be06c871cde92e032ed291a171f103
popere <popere@irontec.com>
2021-12-08 10:12:45 +0100

    fix

--- render/render.py
+++ render/render.py
@@ -173,7 +173,7 @@ class RenderHelper:
             weatherText += '<img class="icon" src="http://openweathermap.org/img/wn/' + weather['daily'][j]['weather'][0]['icon'] +'@2x.png"></img>\n'
             weatherText += '<div>' + str(round(weather['daily'][j]['temp']['max'])) + '°C / ' + str(round(weather['daily'][j]['temp']['min'])) + '°C </div>\n'
             if (('snow' in weather['daily'][j]) and weather['daily'][j]['snow'] != 0):
-              weatherText += '<div>💧/❄️ ' + str(round(weather['daily'][j]['rain'])) + '/' + str(round(weather['daily'][j]['snow'])) + 'mm</div>\n'
+              weatherText += '<div>💧/❄️ ' + str(round(weather['daily'][j]['rain'])) + ' / ' + str(round(weather['daily'][j]['snow'])) + 'mm</div>\n'
             else:
               weatherText += '<div>💧 ' + str(round(weather['daily'][j]['rain'])) + 'mm</div>\n'
             weatherText += '</div>\n'
