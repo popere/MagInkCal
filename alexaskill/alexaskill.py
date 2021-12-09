36d428bdd62bc1626086f08fe6a948215b20120c
popere <popere@irontec.com>
2021-12-09 20:18:59 +0100

    alexaskill

---
+++ alexaskill/alexaskill.py
@@ -0,0 +1,50 @@
+import logging
+import os
+ 
+from flask import Flask
+from flask_ask import Ask, request, session, question, statement
+
+import maginkcal;
+ 
+app = Flask(__name__)
+ask = Ask(app, "/")
+logging.getLogger('flask_ask').setLevel(logging.DEBUG)
+ 
+STATUSON = ["on", "switch on", "enable", "power on", "activate", "turn on"] # all values that are defined as synonyms in type
+STATUSOFF = ["off", "switch off", "disactivate", "turn off", "disable", "turn off"]
+STATUSMAGINKCALENDAR = ["actualizar", "recargar", "mostrar"] # maginkcalendar
+ 
+@ask.launch
+def launch():
+    speech_text = 'Welcome to the Raspberry Pi alexa automation.'
+    return question(speech_text).reprompt(speech_text).simple_card(speech_text)
+ 
+@ask.intent('LightIntent', mapping = {'status':'status'})
+def Calendar(status,room):
+    if status in STATUSMAGINKCALENDAR:
+        return statement('Light was turned on')
+    elif status in STATUSOFF:
+        return statement('Light was turned off')
+    elif status in STATUSMAGINKCALENDAR:
+        maginkcal.main()
+        return statement('Cuadro ha sido acutalizado')
+    else:
+        return statement('Sorry, this command is not possible.')
+ 
+@ask.intent('AMAZON.HelpIntent')
+def help():
+    speech_text = 'You can say hello to me!'
+    return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
+ 
+ 
+@ask.session_ended
+def session_ended():
+    return "{}", 200
+ 
+ 
+if __name__ == '__main__':
+    if 'ASK_VERIFY_REQUESTS' in os.environ:
+        verify = str(os.environ.get('ASK_VERIFY_REQUESTS', '')).lower()
+        if verify == 'false':
+            app.config['ASK_VERIFY_REQUESTS'] = False
+    app.run(debug=True)
