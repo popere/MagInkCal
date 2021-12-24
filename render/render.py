2886fcfca67763eef7e1f0b347b3fb840d01e5bf
popere <popere@irontec.com>
2021-12-24 18:21:20 +0100

    fix

--- render/render.py
+++ render/render.py
@@ -182,7 +182,7 @@ class RenderHelper:
             elif (('rain' in weather['daily'][j]) and weather['daily'][j]['rain'] != 0):
               weatherText += '<div>💧 ' + str(round(weather['daily'][j]['rain'])) + 'mm</div>\n'
             else:
-              weatherText += '<div "no_rain"></div>\n'
+              weatherText += '<div class="no_rain"></div>\n'
             weatherText += '</div>\n'
           weatherText += '</div>\n'
           weatherText += '</div>\n'
